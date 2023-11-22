b9755a523f5b7d51ccad8641280d5cd5c74d1230
rookie <juan.diegoam@hotmail.com>
2023-11-22 09:42:27 -0500

    :building_construction: Thirdweb config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThirdWebProvider } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='bg-gradient-to-tl from-[#2C3639] to-black text-white md:max-w-7xl mx-auto max-w-3xl'>
+        <ThirdWebProvider>
+          {children}
+        </ThirdWebProvider>
+      </body>
     </html>
   )
 }
---
+++ app/providers.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { ThirdwebProvider, ChainId, metamaskWallet, embeddedWallet } from '@thirdweb-dev/react'
+export function ThirdWebProvider({ children }: {
+    children: React.ReactNode
+}) {
+    return (
+        <ThirdwebProvider
+            clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+            activeChain={ChainId.Mumbai}
+            supportedWallets={[
+                metamaskWallet(),
+                embeddedWallet()
+            ]}
+        >
+            {children}
+        </ThirdwebProvider>
+    )
+}
