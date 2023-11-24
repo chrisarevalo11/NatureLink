9b0779242cffd657289545d1f64569d629161bc9
rookie <juan.diegoam@hotmail.com>
2023-11-24 16:47:47 -0500

    :construction: Calling contract with thirdweb

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,36 @@
 'use client'
 
 import { useOwnedHandles, useProfile } from '@lens-protocol/react-web';
+import { Web3Button, useContract, useContractRead } from '@thirdweb-dev/react';
+
+
+
+
+
+
+
 
 export default function Home() {
-  const { data, error, loading } = useProfile({
+ /*  const { data, error, loading } = useProfile({
     forHandle: 'test/rookie',
-  });
+  }); */
 
-  const handle =  useOwnedHandles({
+  const handle = useOwnedHandles({
     for: '0xD496C2D3422F86dCca5b2d7C8728dEDEF6cEE9d0',
   });
-  
+
+  const { data: contract } = useContract("0xB6558651A3A4646D83f1030921909fA87EE61A35");
+  const { data: data2 , isLoading, error } = useContractRead(contract, "getAllProjects");
 
   return (
     <div>
-
+{/* 
       {loading && <p>Cargando perfil...</p>}
       {error && <p>Error al cargar el perfil.</p>}
-      {handle && <h1>{JSON.stringify(handle)}</h1>} 
+      {handle && <h1>{JSON.stringify(handle)}</h1>} */}
+     <button onClick={() => console.log(data2)}>Click me</button>
     </div>
+
   );
 }
 
