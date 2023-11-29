16dea371dfaebcd46081872ffa9f6325f4ad63c5
Christian Arevalo <tiancris200@gmail.com>
2023-11-29 15:50:12 -0500

    :recycle: Refactored code into components

--- components/ProjectPage/Donate.tsx
+++ components/ProjectPage/Donate.tsx
@@ -1,69 +1,22 @@
-"use client";
-
-import { useFormik } from "formik";
-import { ReactNode, useState } from "react";
-import { useDebouncedCallback } from "use-debounce";
+import { ReactNode } from "react";
+import DonationForm from "./DonationForm";
+import ProgressBar from "./ProgressBar";
 
 type DonateProps = {
-  amount: number;
+  RequiredAmount: number;
 };
 
-export default function Donate(): ReactNode {
-  const [values, setValues] = useState<DonateProps>({
-    amount: 0,
-  });
-
-  const handleChange = useDebouncedCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value }: { name: string; value: string } = e.target;
-      setValues({
-        ...values,
-        [name]: parseInt(value),
-      });
-    },
-    1000
-  );
-
-  const formik = useFormik({
-    initialValues: values,
-    onSubmit: () => {
-      console.log(values);
-    },
-  });
+export default function Donate({ RequiredAmount }: DonateProps): ReactNode {
+  const currentAmount: number = 11000;
 
   return (
     <div className="w-full flex flex-col gap-5">
-      <h1 className="text-3xl font-bold">Donate</h1>
-      <form
-        onSubmit={formik.handleSubmit}
-        className="w-full flex flex-col items-center gap-5"
-      >
-        <div className="flex w-[90%]">
-          <input
-            name="amount"
-            type="number"
-            onChange={handleChange}
-            className="input w-[90%] rounded-r-none bg-gray-700"
-            placeholder="Amount"
-          />
-          <span className="px-5 bg-gray-800 flex items-center rounded-r-full">
-            MATIC
-          </span>
-        </div>
-        <button className="btn btn-primary btn-wide" type="submit">
-          {" "}
-          Donate
-        </button>
-      </form>
-      <div>
-        <progress
-          className="progress progress-primary w-56"
-          value={1378}
-          max={"100"}
-        ></progress>
-        <h3></h3>
-        <h3></h3>
-      </div>
+      <h1 className="text-3xl font-bold text-center">Donate</h1>
+      <DonationForm />
+      <ProgressBar
+        currentAmount={currentAmount}
+        RequiredAmount={RequiredAmount}
+      />
     </div>
   );
 }
