4b288eee09faeb8e40e259ceff0d44b4b4bb552e
Santiago Viana <salviega6@gmail.com>
2023-12-07 15:22:57 -0500

    :sparkles: Proposals are fetched

--- components/ExplorePage/ExploreTabs.tsx
+++ components/ExplorePage/ExploreTabs.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import { ReactElement, ReactNode, useEffect, useState } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import ExploreProjects from './ExploreProjects'
 import ExploreHypercerts from './ExploreHypercerts'
 import { useAppSelector } from '@/store'
-import { useDispatch } from 'react-redux'
-import { getAllProjectsReturnDtoToGetAllProjectsReturn } from '@/functions/dtos/dtos'
+import { Propousal } from '@/models/contract-functions-args.model'
+import { getContract } from '@thirdweb-dev/sdk'
 import { useContract, useContractRead } from '@thirdweb-dev/react'
-import { setProjects } from '@/store/slides/projectsSlide'
-import natureLinkJson from '@/deployments/mumbai/NatureLink.json'
-import { Project } from '@/models/contract-functions-args.model'
 
 const tabs: string[] = ['Projects', 'Hypercerts']
 const tabContent: Record<string, ReactElement> = {
@@ -19,17 +16,10 @@ const tabContent: Record<string, ReactElement> = {
 
 export default function ExploreTabs(): JSX.Element {
 	const [activeTabIndex, setActiveTabIndex] = useState<number>(0)
-	const [isSpinning, setIsSpinning] = useState<boolean>(false)
-	const dispatch = useDispatch()
-	const projects = useAppSelector(state => state.projects.projects)
 
-	const { data: contract } = useContract(natureLinkJson.address)
-
-	const {
-		data: getAllProjects,
-		isLoading,
-		error
-	} = useContractRead(contract, 'getAllProjects')
+	const propousals: Propousal[] = useAppSelector(
+		state => state.propousal.propousals
+	)
 
 	const currentTab: string = tabs[activeTabIndex]
 
@@ -37,47 +27,30 @@ export default function ExploreTabs(): JSX.Element {
 		setActiveTabIndex(index)
 	}
 
-	useEffect(() => {
-		if (projects.length === 0) {
-			setIsSpinning(true)
-			if (!isLoading) {
-				const currentProjects: Project[] =
-					getAllProjectsReturnDtoToGetAllProjectsReturn(getAllProjects)
-
-				dispatch(setProjects(currentProjects))
-				setIsSpinning(false)
-			}
-		}
-	}, [isLoading])
+	useEffect(() => {}, [])
 
 	return (
 		<>
-			{isSpinning ? (
-				<p>Cargando proyectos...</p>
-			) : (
-				<>
-					<div role='tablist' className='tabs tabs-boxed w-full bg-gray-900'>
-						{tabs.map((tab, index) => (
-							<a
-								key={index}
-								role='tab'
-								className={`tab  ${
-									activeTabIndex === index && 'tab-active pointer-events-none'
-								}`}
-								onClick={() => handleTabClick(index)}
-							>
-								{tab}
-							</a>
-						))}
-					</div>
-					<div
-						style={{ marginTop: '10px' }}
-						className='flex justify-center w-full items-center p-2'
+			<div role='tablist' className='tabs tabs-boxed w-full bg-gray-900'>
+				{tabs.map((tab, index) => (
+					<a
+						key={index}
+						role='tab'
+						className={`tab  ${
+							activeTabIndex === index && 'tab-active pointer-events-none'
+						}`}
+						onClick={() => handleTabClick(index)}
 					>
-						{tabContent[currentTab]}
-					</div>
-				</>
-			)}
+						{tab}
+					</a>
+				))}
+			</div>
+			<div
+				style={{ marginTop: '10px' }}
+				className='flex justify-center w-full items-center p-2'
+			>
+				{tabContent[currentTab]}
+			</div>
 		</>
 	)
 }
