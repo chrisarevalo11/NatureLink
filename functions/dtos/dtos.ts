2166076308697759a655c798bfc26e3b5ee17f04
Santiago Viana <salviega6@gmail.com>
2023-12-07 15:20:56 -0500

    :sparkles: News dtos

--- functions/dtos/dtos.ts
+++ functions/dtos/dtos.ts
@@ -1,14 +1,16 @@
 import { BigNumber, ethers } from 'ethers'
 import {
 	BigIntDto,
-	Project,
-	ProjectDto
+	Propousal,
+	PropousalDto,
+	Stake,
+	StakeDto
 } from '@/models/contract-functions-args.model'
 
-export function getAllProjectsReturnDtoToGetAllProjectsReturn(
-	projects: ProjectDto[]
-): Project[] {
-	return projects.map((projectDto: any) => {
+export function propousalDtoToPropousal(
+	projectsDto: PropousalDto[]
+): Propousal[] {
+	return projectsDto.map((projectDto: any) => {
 		const idDto: BigIntDto = projectDto[0]
 		const idBN: BigNumber = BigNumber.from(idDto._hex)
 		let id: string | number = idBN.toNumber()
@@ -40,7 +42,7 @@ export function getAllProjectsReturnDtoToGetAllProjectsReturn(
 		const evaluationAddressDto: string = projectDto[6]
 		const creatorAddressDto: string = projectDto[7]
 
-		const project: Project = {
+		const project: Propousal = {
 			id: id,
 			amount: amount,
 			projectTimeStart: projectTimeStart,
@@ -54,4 +56,54 @@ export function getAllProjectsReturnDtoToGetAllProjectsReturn(
 
 		return project
 	})
+}
+
+export function stakeDtoToStake(stakeDto: StakeDto): Stake {
+	const openForStakeDto: boolean = stakeDto.openForStake
+	const openForWithdrawDto: boolean = stakeDto.openForWithdraw
+
+	const bountyBN: BigNumber = BigNumber.from(stakeDto.bounty._hex)
+	let bounty: string | number = ethers.utils.formatEther(bountyBN)
+	bounty = Number(bounty)
+
+	const feeDto: BigNumber = BigNumber.from(stakeDto.fee._hex)
+	let fee: string | number = ethers.utils.formatEther(feeDto)
+	fee = Number(fee)
+
+	const deadlineBn: BigNumber = BigNumber.from(stakeDto.deadline._hex)
+	const deadline = deadlineBn.toNumber()
+	// TODO: Convert to date
+
+	const tokenIdCounterBn: BigNumber = BigNumber.from(
+		stakeDto.tokenIdCounter._hex
+	)
+	const tokenIdCounter: number = tokenIdCounterBn.toNumber()
+
+	const thresholdBn: BigNumber = BigNumber.from(stakeDto.threshold._hex)
+	const threshold: number = thresholdBn.toNumber()
+	// TODO: Convert to date
+
+	const creatorDto: string = stakeDto.creator
+	const treasuryAddressDto: string = stakeDto.treasuryAddress
+	const hypercertsAddressDto: string = stakeDto.hypercertsAddress
+	const pushCommAddressDto: string = stakeDto.pushCommAddress
+	const stakersDto: string[] = stakeDto.stakers
+	const infoDto: string = stakeDto.info
+
+	const stake: Stake = {
+		openForStake: openForStakeDto,
+		openForWithdraw: openForWithdrawDto,
+		bounty: bounty,
+		fee: fee,
+		deadline: deadline,
+		tokenIdCounter: tokenIdCounter,
+		threshold: threshold,
+		creator: creatorDto,
+		treasuryAddress: treasuryAddressDto,
+		hypercertsAddress: hypercertsAddressDto,
+		pushCommAddress: pushCommAddressDto,
+		stakers: stakersDto,
+		info: infoDto
+	}
+	return stake
 }
