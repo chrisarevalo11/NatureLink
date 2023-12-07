edfffd757ad8a6b4bcba20232a4e2c6eef0e6b44
Christian Arevalo <tiancris200@gmail.com>
2023-12-07 15:37:19 -0500

    :fire: Removed unused files

--- app/api/readContract/route.ts
+++
@@ -1,9 +0,0 @@
-import { Propousal } from '@/models/contract-functions-args.model'
-import { getProposals } from '@/services/projects.services'
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export async function GET(req: Request, res: Response) {
-	const proposals: Propousal[] = await getProposals()
-	// console.log("projects: ", projects);
-	return new Response('Hello World')
-}
