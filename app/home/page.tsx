fd9b47c48930c5bf863774b054d35a30c0b6e2bb
rookiecol <juan.diegoam@hotmail.com>
2023-12-08 16:24:55 -0500

    :construction: Lens hooks onboarding

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,21 @@
-"use client";
+'use client'
 
-import React, { ReactNode } from "react";
-import Feed from "@/components/FeedPage/Feed";
+import React, { ReactNode } from 'react'
+import Feed from '@/components/FeedPage/Feed'
+import {
+	ExplorePublicationType,
+	ExplorePublicationsOrderByType,
+	useExplorePublications
+} from '@lens-protocol/react-web'
 
 export default function Home(): ReactNode {
-  return <Feed />;
+	const { data: publications } = useExplorePublications({
+		where: {
+			publicationTypes: [ExplorePublicationType.Post]
+		},
+		orderBy: ExplorePublicationsOrderByType.TopCommented
+	})
+
+	console.log(publications)
+	return <Feed />
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,49 @@
 'use client'
-
-import React, { useEffect } from 'react';
-import { useLogin } from '@lens-protocol/react-web';
-import { useAddress } from "@thirdweb-dev/react";
-
+import { useClaimHandle, useLogin } from '@lens-protocol/react-web'
+import { useAddress } from '@thirdweb-dev/react'
 
 export default function Home() {
+	const address = useAddress()
+	const { execute: executeLogin, loading, data, error } = useLogin()
+	const { execute: executeClaimHandle } = useClaimHandle()
 
-  
-  const { execute, loading, data, error } = useLogin();
-  const address = useAddress(); // Retrieves the user's wallet address
+	const handleClaim = async () => {
+		if (address) {
+			try {
+				const result = await executeClaimHandle({ localName: 'test777' })
+				console.log(result)
+			} catch (err) {
+				console.error(err)
+			}
+		} else {
+			console.error('Wallet address not found. Please connect your wallet.')
+		}
+	}
 
-  const login = async () => {
-    if (address) {
-      const result = await execute({ address });
-      // Handle the result if needed
-    } else {
-      // Handle the case where the address is not available
-      console.log(error);
-      console.error("Wallet address not found. Please connect your wallet.");
-    }
-  };
-  
-  return (
-    <div>
-      {loading && <p>Cargando perfil...</p>}
-      {error && <p>Error al cargar el perfil: {error.message}</p>}
-      {data && <h1>{JSON.stringify(data)}</h1>}
-      <button onClick={login}>Click me</button>
-    </div>
-  );
+	const login = async () => {
+		if (address) {
+			try {
+				const result = await executeLogin({ address })
+				console.log(result)
+			} catch (err) {
+				console.error('Error during login:', err)
+			}
+		} else {
+			console.error('Wallet address not found. Please connect your wallet.')
+		}
+	}
+
+	return (
+		<div>
+			{loading && <p>Cargando perfil...</p>}
+			{error && <p>Error al cargar el perfil: {error.message}</p>}
+			{data && <h1>{JSON.stringify(data)}</h1>}
+			<button className={'btn btn-primary'} onClick={login}>
+				Login
+			</button>
+			<button className={'btn btn-primary'} onClick={handleClaim}>
+				Claim Handle
+			</button>
+		</div>
+	)
 }
-
