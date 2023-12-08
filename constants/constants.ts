a3332579e66a170b0d5eec054f2d96af71a93e40
Santiago Viana <salviega6@gmail.com>
2023-12-08 16:36:39 -0500

    :truck: Was moved test object

---
+++ constants/constants.ts
@@ -0,0 +1,16 @@
+import { formValuesTypes } from '@/app/create/page'
+
+export const test: formValuesTypes = {
+	projectName: 'Project Name',
+	bannerImage:
+		'https://pbs.twimg.com/media/F_KOERzXkAAaXFC?format=jpg&name=small',
+	logo: 'https://pbs.twimg.com/profile_images/1494316842503925764/eJ-0xwBV_400x400.jpg',
+	link: 'https://www.google.com',
+	description:
+		'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+	amount: 18000,
+	startDate: '2024-01-01',
+	endDate: '2022-02-01',
+	scopeTags: 'tag1, tag2, tag3',
+	contributors: '0x12e3e1, Juan, Rookie'
+}
