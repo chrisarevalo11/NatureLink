58202c7da440b369f218f1a30ca910f6cc9e78e6
Christian Arevalo <tiancris200@gmail.com>
2023-12-09 09:06:30 -0500

    :construction: Work in progress

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,20 +2,7 @@
 
 import React, { ReactNode } from 'react'
 import Feed from '@/components/FeedPage/Feed'
-import {
-	ExplorePublicationType,
-	ExplorePublicationsOrderByType,
-	useExplorePublications
-} from '@lens-protocol/react-web'
 
 export default function Home(): ReactNode {
-	const { data: publications } = useExplorePublications({
-		where: {
-			publicationTypes: [ExplorePublicationType.Post]
-		},
-		orderBy: ExplorePublicationsOrderByType.TopCommented
-	})
-
-	console.log(publications)
 	return <Feed />
 }
--- components/FeedPage/ShareAPost.tsx
+++ components/FeedPage/ShareAPost.tsx
@@ -1,48 +1,56 @@
+import { useCreatePost } from '@lens-protocol/react-web'
+import { useFormik } from 'formik'
 import {
-  ChangeEventHandler,
-  MutableRefObject,
-  ReactElement,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+	ChangeEventHandler,
+	MutableRefObject,
+	ReactElement,
+	useEffect,
+	useRef,
+	useState
+} from 'react'
 
 export default function ShareAPost(): ReactElement {
-  const textAreaRef: MutableRefObject<HTMLTextAreaElement | null> =
-    useRef(null);
-  const [content, setContent] = useState<String>("");
+	const textAreaRef: MutableRefObject<HTMLTextAreaElement | null> = useRef(null)
+	const [content, setContent] = useState<String>('')
+	const { execute, error, loading } = useCreatePost()
 
-  const maxLength = 250;
+	const maxLength = 250
 
-  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    setContent(e.target.value);
-  };
+	const handleChange: ChangeEventHandler<HTMLTextAreaElement> = e => {
+		setContent(e.target.value)
+	}
 
-  useEffect(() => {
-    if (textAreaRef.current) {
-      textAreaRef.current.style.height = "auto";
-      textAreaRef.current.style.height =
-        textAreaRef.current.scrollHeight + "px";
-    }
-  }, [content]);
+	useEffect(() => {
+		if (textAreaRef.current) {
+			textAreaRef.current.style.height = 'auto'
+			textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px'
+		}
+	}, [content])
 
-  return (
-    <div className="p-2 bg-gray-800 rounded-lg">
-      <textarea
-        className="w-full p-2 rounded-lg bg-gray-700 outline-gray-600 resize-none"
-        placeholder="What's on your mind?"
-        value={content as any}
-        onChange={handleChange}
-        rows={2}
-        ref={textAreaRef}
-        maxLength={maxLength}
-      />
-      <button
-        className={`btn mt-2 ${!content && "pointer-events-none opacity-40"}`}
-        type="submit"
-      >
-        Post
-      </button>
-    </div>
-  );
+	const formik = useFormik({
+		initialValues: content,
+		onSubmit: async () => {}
+	})
+
+	return (
+		<div className='w-full p-2 bg-gray-800 rounded-lg'>
+			<form onSubmit={formik.handleSubmit}>
+				<textarea
+					className='w-full p-2 rounded-lg bg-gray-700 outline-gray-600 resize-none'
+					placeholder="What's on your mind?"
+					value={content as any}
+					onChange={handleChange}
+					rows={2}
+					ref={textAreaRef}
+					maxLength={maxLength}
+				/>
+				<button
+					className={`btn mt-2 ${!content && 'pointer-events-none opacity-40'}`}
+					type='submit'
+				>
+					Post
+				</button>
+			</form>
+		</div>
+	)
 }
